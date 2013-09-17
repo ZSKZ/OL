3e35ea90bae10f4300a4b47a44fbabf6cad95841
playlaugh <playlaugh2011@gmail.com>
2013-09-17 14:36:50 +0800

    1.2
    server's 2

---
+++ app/models/qa/FocusQues.java
@@ -0,0 +1,24 @@
+package models.qa;
+
+import play.*;
+import play.db.jpa.*;
+
+import javax.persistence.*;
+import java.util.*;
+
+@Entity
+public class FocusQues extends Model {
+	public String userType;
+	public Long userid;
+	public String userprofile;
+	public Long quesId;
+
+	public FocusQues(String userType, Long userid, String userprofile,
+			Long quesId) {
+		this.userid = userid;
+		this.userprofile = userprofile;
+		this.userType = userType;
+		this.quesId = quesId;
+		create();
+	}
+}
